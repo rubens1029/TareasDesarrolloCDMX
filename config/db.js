d5bd4013065c64a30cd14171557101d3f0401053
Ruben Garcia <1029rubens@gmail.com>
2020-10-06 14:17:32 -0500

    Cambio en el db.js heroku

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const Sequelize = require('sequelize');
 //importar variables
 require('dotenv').config({ path: '.env'});
 
-console.log("heroku_54c5bab1cdcf6d1", process.env.BD_USER, process.env.BD_PASS, process.env.BD_HOST,process.env.BD_PORT)
+console.log(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, process.env.BD_HOST,process.env.BD_PORT)
 
-const db = new Sequelize("heroku_54c5bab1cdcf6d1", process.env.BD_USER, process.env.BD_PASS, {
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
   host: process.env.BD_HOST,
   dialect: 'mysql',
   port: process.env.BD_PORT,
